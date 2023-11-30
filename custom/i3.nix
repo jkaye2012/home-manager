e2c2427cfdfd8f00a5648e9a476ac806c34c2394
Jordan Kaye <jkaye@belvederetrading.com>
2023-11-30 11:16:16 -0700

    Adds screenshots and clipboard management

--- custom/i3.nix
+++ custom/i3.nix
@@ -18,10 +18,12 @@ in {
         startup = [
           { command = "xmodmap ~/.xmodmap"; always = true; notification = false; }
           { command = "${pkgs.networkmanagerapplet}/bin/nm-applet"; always = true; notification = false; }
+          { command = "${pkgs.xfce.xfce4-clipman-plugin}/bin/xfce4-clipman"; always = true; notification = false; }
         ];
 
         keybindings = lib.mkOptionDefault {
           "${mod}+d" = "exec ${pkgs.dmenu}/bin/dmenu_run";
+          "Print" = "exec ${pkgs.xfce.xfce4-screenshooter}/bin/xfce4-screenshooter -c -r -s /home/jkaye/Pictures/Screenshots";
           # "${mod}+x" = "exec sh -c '${pkgs.maim}/bin/maim -s | xclip -selection clipboard -t image/png'";
           # "${mod}+Shift+x" = "exec sh -c '${pkgs.i3lock}/bin/i3lock -c 222222 & sleep 5 && xset dpms force of'";
 
